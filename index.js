74d0085620c1921f6b4df7a620a3c006a24515a8
Alia-elsaeid <aliaghbon2810@gmail.com>
2024-12-01 15:40:20 +0000

    Update index.js using init recommendation

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const http = require("http");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = "Hello Node!\n";  // Double quotes and semicolon added
   res.end(msg);
 });
 
 server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+  console.log(`Server running on http://localhost:${port}/`);  // Double quotes
 });
